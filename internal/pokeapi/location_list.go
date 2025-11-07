f0db27eb5a9c770d959feef6a5473bbdb1d7c73c
ericminnick <minnick.eric@gmail.com>
2025-11-07 10:07:50 -0600

    fixed naming in pokeapi

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -6,41 +6,41 @@ import (
 	"net/http"
 )
 
-func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
+func (c *Client) ListLocations(pageURL *string) (mapResponse, error) {
 	url := baseURL + "/location-area"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
 	if val, ok := c.cache.Get(url); ok {
-		locationsResp := RespShallowLocations{}
+		locationsResp := mapResponse{}
 		err := json.Unmarshal(val, &locationsResp)
 		if err != nil {
-			return RespShallowLocations{}, err
+			return mapResponse{}, err
 		}
 		return locationsResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil) 
 	if err != nil {
-		return RespShallowLocations{}, err 
+		return mapResponse{}, err 
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return mapResponse{}, err
 	}
 	defer resp.Body.Close()
 
 	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return mapResponse{}, err
 	}
 
-	locationsResp := RespShallowLocations{}
+	locationsResp := mapResponse{}
 	err = json.Unmarshal(dat, &locationsResp)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return mapResponse{}, err
 	}
 
 	c.cache.Add(url, dat)
